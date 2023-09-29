6de62aec40d3d601baaf95a329092e4b477287a5
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-09-28 23:11:03 -0500

    Added initial page and ticker addition

--- stocks.py
+++ stocks.py
@@ -4,6 +4,7 @@ from selenium import webdriver                                    #
 from selenium.webdriver.chrome.service import Service             #
 from selenium.webdriver.support.ui import WebDriverWait           #
 from selenium.webdriver.common.by import By                       #
+from selenium.webdriver.common.keys import Keys                   #
                                                                   #
 _service = Service()                                              #
 _options = webdriver.ChromeOptions()                              #
@@ -23,21 +24,55 @@ logging.basicConfig(level=logging.CRITICAL)                       #
 ###################################################################
 
 
+
+#### Load url for main site ####
 def loadUrl(url):
     driver.get(url)
 
-    print('---- loading URL      ----')
+    print('---- loading URL         ----')
 
     # Give 30 seconds to fully load the URL
     WebDriverWait(driver, 30).until(
         lambda driver: driver.execute_script("return document.readyState") == "complete"
     )
 
-    print('---- URL fully loaded ----')
+    print('---- URL fully loaded    ----')
 
+
+#### Load ticker ####
+def loadTicker(ticker):
+    _initial_search_bar = driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div[2]/div[2]/div/div/button[1]/span')
+
+    _initial_search_bar.click()
+
+    time.sleep(2)
+
+    print('---- loading ticker      ----')
+
+    _real_search_bar = driver.find_element(By.XPATH, '//*[@id="overlap-manager-root"]/div/div/div[2]/div/div/div[1]/div/div[1]/span/form/input')
+
+    _real_search_bar.send_keys(ticker)
+
+    time.sleep(2)
+
+    _real_search_bar.send_keys(Keys.ENTER)
+
+    # Give 30 seconds to fully load the URL
+    WebDriverWait(driver, 30).until(
+        lambda driver: driver.execute_script("return document.readyState") == "complete"
+    )
+
+    print('---- ticker fully loaded ----')
+
+    time.sleep(3)
 
 def main():
-    loadUrl('http://www.google.com')
+
+    # Trading View
+    loadUrl('http://www.tradingview.com/screener/')
+
+    # Ticker
+    loadTicker('AAPL')
 
     driver.quit()
 
